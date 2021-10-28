58a6225e3e2f9f9f249bcba88bc65a85e43384e0
chrisgeor <georgiouchristos8@gmail.com>
2021-10-28 19:10:11 +0300

    Add Demo v.0.1

---
+++ src/gr/aueb/elearn/ch1/AddDemo.java
@@ -0,0 +1,21 @@
+package gr.aueb.elearn.ch1;
+
+public class AddDemo {
+
+    public static void main(String[] args) {
+    // Dilwsi kai arxikopoihsh metablitwn
+        /*
+        ta onomata twn identifiers ksekinane me mikro gramma en antithesi me twn klasewn pu ksekinane
+        me kefalaio prwto gramma
+         */
+    int a=8;
+    int b=4;
+    int sum;
+    //entoles
+    sum=a+b;
+    //ektypwsi apotelesmatwn
+    System.out.println("Το άθροισμα των "+a+"+"+b+" είναι "+sum);
+    System.out.println();
+    System.out.printf("To athroisma twn %d+%d ine %d",a,b,sum);
+}
+}
